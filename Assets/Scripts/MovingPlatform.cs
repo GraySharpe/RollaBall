d1dcefc81c5b39bafbffaccf0c4267fafc44a958
GraySharpe <grayson.sharpe@shaw.ca>
2015-04-21 21:55:16 -0700

    [RollaBall] -Made platform move and ball stick to platform under the MovingPlatform script.

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovingPlatform : MonoBehaviour {
+public class MovingPlatform : MonoBehaviour 
+{
+	public Transform DestinationSpot;
+	public Transform OriginSpot;
+	public float Speed;
+	public bool Switch = false;
 
-	// Use this for initialization
-	void Start () {
-	
+	void FixedUpdate()
+	{
+		//for these 2 if statements, it's checking the position of the platform.
+		//if it's at the destination spot, it sets the Switch to true.
+		if(transform.position == DestinationSpot.position)
+		{
+			Switch = true;
+		}
+		if(transform.position == OriginSpot.position)
+		{
+			Switch = false;
+		}
+
+		// if switch becomes true, it tells the platform to move to its origin.
+		if(Switch)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, OriginSpot.position, Speed);
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, DestinationSpot.position, Speed);
+
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.collider.CompareTag("Player"))
+		{
+			collision.gameObject.transform.parent = this.transform;
+		}
+	}
+	void OnCollisionExit(Collision collision)
+	{
+		if(collision.collider.CompareTag("Player"))
+		{
+			collision.gameObject.transform.parent = null;
+		}
 	}
 }
+
