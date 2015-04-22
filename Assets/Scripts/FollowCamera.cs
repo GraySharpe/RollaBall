86cd43bdc4c5671669cf954cd39eaf4891c0f82b
GraySharpe <grayson.sharpe@shaw.ca>
2015-04-21 20:02:05 -0700

    [RollaBall] -Initial source file

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	public GameObject target; 		// view target
+	Vector3 offset; 				// position reletive to our view target
+
+	void Start () 
+	{
+		// compute the offset relative to our target
+		offset = transform.position - target.transform.position;
+	}
+	
+
+	void Update () 
+	{
+		transform.position = target.transform.position + offset;
+	}
+}
